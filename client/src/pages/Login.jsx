0d9886a822e633fe1b15e4c4d1bd3ac6aaa9c741
sri akshaya <akshayarsri@gmail.com>
2026-07-07 16:28:31 +0530

    Created Login page in frontend

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,35 @@
+import {useState} from "react";
+import {useNavigate,Link} from "react-route-dom";
+import api from "../api";
+
+function Login(){
+    const navigate = useNavigate();
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+
+    const handleLogin = async (e)=>{
+        e.preventDefault();
+        try{
+            const res = await api.post("auth/login",{
+                email,password
+            });
+            localStorage.setItem("token",res.data.token);
+            alert("Login Successful");
+            navigate("/dashboard");
+        }catch(err){
+            alert(err.response?.data?.message || "Login Failed");
+        }
+    }
+    return (
+        <div className = "containe">
+            <h2>Login</h2>
+            <form onSubmit = {handleLogin}>
+                <input type="email" placeholder="Email" value={email} onChange={(e)=>setEmail(e.target.value)} required/>
+                <input type="password" placeholder="Password" value={password} onChange={(e)=>setPassword(e.target.value)} required/>
+                <button type="submit">Login</button>
+            </form>
+            <p>Dont have an account?{" "}<Link to="/register">Register</Link></p>
+        </div>
+    );
+}
+export default Login;
